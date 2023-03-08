a1a86be3e906ced1c246e4b48c6f952b68473a53
deusebio <enrico.deusebio@canonical.com>
2023-03-08 14:42:07 +0100

    [MISC] Upgrade to Jammy 22.04 (#32)

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -8,5 +8,5 @@ import yaml
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
 ZOOKEEPER_IMAGE = METADATA["resources"]["zookeeper-image"]["upstream-source"]
-SERIES = "focal"
+SERIES = "jammy"
 TLS_OPERATOR_SERIES = "jammy"
