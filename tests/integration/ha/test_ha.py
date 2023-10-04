ac18ef7f3932f41a82f35e67bdf76601a8362fb3
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-10-05 00:45:03 +0100

    temp: move network-cut last

--- tests/integration/ha/test_ha.py
+++ tests/integration/ha/test_ha.py
@@ -290,74 +290,6 @@ async def test_freeze_db_process(ops_test: OpsTest, request):
 
 
 @pytest.mark.abort_on_fail
-async def test_network_cut_without_ip_change(ops_test: OpsTest, request, chaos_mesh):
-    """Cuts and restores network on leader, cluster self-heals after IP change."""
-    hosts = helpers.get_hosts(ops_test)
-    leader_name = helpers.get_leader_name(ops_test, hosts)
-    initial_leader_host = helpers.get_unit_host(ops_test, leader_name)
-    password = helpers.get_super_password(ops_test)
-    parent = request.node.name
-    non_leader_hosts = ",".join([host for host in hosts.split(",") if host != initial_leader_host])
-
-    logger.info("Starting continuous_writes...")
-    cw.start_continuous_writes(
-        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
-    )
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # letting client set up and start writing
-
-    logger.info("Checking writes are running at all...")
-    assert cw.count_znodes(
-        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
-    )
-
-    logger.info("Cutting leader network...")
-    helpers.isolate_instance_from_cluster(ops_test, leader_name)
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)
-    logger.info("Checking writes are increasing...")
-    writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # increasing writes
-    new_writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    assert new_writes > writes, "writes not continuing to ZK"
-
-    logger.info("Checking leader re-election...")
-    new_leader_name = helpers.get_leader_name(ops_test, non_leader_hosts)
-    assert new_leader_name != leader_name
-
-    logger.info("Restoring leader network...")
-    helpers.remove_instance_isolation(ops_test)
-
-    # livenessProbe should fail after 10*3 seconds by default, then the container is restarted
-    # allows dead ZK server to rejoin quorum
-    await asyncio.sleep(CLIENT_TIMEOUT * 6)
-
-    logger.info("Stopping continuous_writes...")
-    cw.stop_continuous_writes()
-
-    logger.info("Counting writes on surviving units...")
-    last_write = cw.get_last_znode(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    total_writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    assert last_write == total_writes
-
-    logger.info("Checking old leader caught up...")
-    last_write_leader = cw.get_last_znode(
-        parent=parent, hosts=initial_leader_host, username=helpers.USERNAME, password=password
-    )
-    total_writes_leader = cw.count_znodes(
-        parent=parent, hosts=initial_leader_host, username=helpers.USERNAME, password=password
-    )
-    assert last_write == last_write_leader
-    assert total_writes == total_writes_leader
-
-
-@pytest.mark.abort_on_fail
 async def test_full_cluster_crash(ops_test: OpsTest, request):
     hosts = helpers.get_hosts(ops_test)
     leader_name = helpers.get_leader_name(ops_test, hosts)
@@ -489,3 +421,73 @@ async def test_full_cluster_restart(ops_test: OpsTest, request):
         parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
     )
     assert last_write == total_writes
+
+
+@pytest.mark.abort_on_fail
+async def test_network_cut_without_ip_change(ops_test: OpsTest, request, chaos_mesh):
+    """Cuts and restores network on leader, cluster self-heals after IP change."""
+    hosts = helpers.get_hosts(ops_test)
+    leader_name = helpers.get_leader_name(ops_test, hosts)
+    initial_leader_host = helpers.get_unit_host(ops_test, leader_name)
+    password = helpers.get_super_password(ops_test)
+    parent = request.node.name
+    non_leader_hosts = ",".join([host for host in hosts.split(",") if host != initial_leader_host])
+
+    logger.info("Starting continuous_writes...")
+    cw.start_continuous_writes(
+        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
+    )
+    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # letting client set up and start writing
+
+    logger.info("Checking writes are running at all...")
+    assert cw.count_znodes(
+        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
+    )
+
+    logger.info("Cutting leader network...")
+    helpers.isolate_instance_from_cluster(ops_test, leader_name)
+    await asyncio.sleep(CLIENT_TIMEOUT * 3)
+    logger.info("Checking writes are increasing...")
+    writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
+    )
+    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # increasing writes
+    new_writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
+    )
+    assert new_writes > writes, "writes not continuing to ZK"
+
+    logger.info("Checking leader re-election...")
+    new_leader_name = helpers.get_leader_name(ops_test, non_leader_hosts)
+    assert new_leader_name != leader_name
+
+    logger.info("Restoring leader network...")
+    helpers.remove_instance_isolation(ops_test)
+
+    # livenessProbe should fail after 10*3 seconds by default, then the container is restarted
+    # allows dead ZK server to rejoin quorum
+    await asyncio.sleep(CLIENT_TIMEOUT * 6)
+
+    logger.info("Stopping continuous_writes...")
+    cw.stop_continuous_writes()
+
+    logger.info("Counting writes on surviving units...")
+    last_write = cw.get_last_znode(
+        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
+    )
+    total_writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
+    )
+    assert last_write == total_writes
+
+    logger.info("Checking old leader caught up...")
+    last_write_leader = cw.get_last_znode(
+        parent=parent, hosts=initial_leader_host, username=helpers.USERNAME, password=password
+    )
+    total_writes_leader = cw.count_znodes(
+        parent=parent, hosts=initial_leader_host, username=helpers.USERNAME, password=password
+    )
+    assert last_write == last_write_leader
+    assert total_writes == total_writes_leader
+
+
