f503769b492c2b75180ccd9c867b14b5d486f7b1
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-10-04 13:02:49 +0100

    chore: fix wonky rebase

--- tests/integration/ha/test_ha.py
+++ tests/integration/ha/test_ha.py
@@ -94,192 +94,6 @@ async def test_kill_db_process(ops_test: OpsTest, request):
 
 
 @pytest.mark.abort_on_fail
-async def test_freeze_db_process(ops_test: OpsTest, request):
-    """SIGSTOPs leader process and checks recovery + re-election after SIGCONT."""
-    hosts = helpers.get_hosts(ops_test)
-    leader_name = helpers.get_leader_name(ops_test, hosts)
-    leader_host = helpers.get_unit_host(ops_test, leader_name)
-    password = helpers.get_super_password(ops_test)
-    parent = request.node.name
-    non_leader_hosts = ",".join([host for host in hosts.split(",") if host != leader_host])
-
-    logger.info("Starting continuous_writes...")
-    cw.start_continuous_writes(
-        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
-    )
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # letting client set up and start writing
-
-    logger.info("Checking writes are running at all...")
-    assert cw.count_znodes(
-        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
-    )
-
-    logger.info("Stopping leader process...")
-    await helpers.send_control_signal(ops_test=ops_test, unit_name=leader_name, signal="SIGSTOP")
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # to give time for re-election
-
-    assert await helpers.process_stopped(ops_test, leader_name)
-
-    logger.info("Checking writes are increasing...")
-    writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # increasing writes
-    new_writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    assert new_writes > writes, "writes not continuing to ZK"
-
-    logger.info("Checking leader re-election...")
-    new_leader_name = helpers.get_leader_name(ops_test, non_leader_hosts)
-    assert new_leader_name != leader_name
-
-    logger.info("Continuing leader process...")
-    await helpers.send_control_signal(ops_test=ops_test, unit_name=leader_name, signal="SIGCONT")
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # letting writes continue while unit rejoins
-
-    logger.info("Stopping continuous_writes...")
-    cw.stop_continuous_writes()
-    await asyncio.sleep(CLIENT_TIMEOUT)  # buffer to ensure writes sync
-
-    logger.info("Counting writes on surviving units...")
-    last_write = cw.get_last_znode(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    total_writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    assert last_write == total_writes
-
-    logger.info("Checking old leader caught up...")
-    last_write_leader = cw.get_last_znode(
-        parent=parent, hosts=leader_host, username=helpers.USERNAME, password=password
-    )
-    total_writes_leader = cw.count_znodes(
-        parent=parent, hosts=leader_host, username=helpers.USERNAME, password=password
-    )
-    assert last_write == last_write_leader
-    assert total_writes == total_writes_leader
-
-
-@pytest.mark.abort_on_fail
-async def test_two_clusters_not_replicated(ops_test: OpsTest, request):
-    """Confirms that writes to one cluster are not replicated to another."""
-    zk_2 = f"{helpers.APP_NAME}2"
-
-    logger.info("Deploying second cluster...")
-    new_charm = await ops_test.build_charm(".")
-    await ops_test.model.deploy(
-        new_charm,
-        application_name=zk_2,
-        num_units=3,
-        resources={"zookeeper-image": helpers.ZOOKEEPER_IMAGE},
-        series=helpers.SERIES,
-    )
-    await helpers.wait_idle(ops_test, apps=[helpers.APP_NAME, zk_2])
-
-    parent = request.node.name
-
-    hosts_1 = helpers.get_hosts(ops_test)
-    hosts_2 = helpers.get_hosts(ops_test, app_name=zk_2)
-    password_1 = helpers.get_super_password(ops_test)
-    password_2 = helpers.get_super_password(ops_test, app_name=zk_2)
-
-    logger.info("Starting continuous_writes on original cluster...")
-    cw.start_continuous_writes(
-        parent=parent, hosts=hosts_1, username=helpers.USERNAME, password=password_1
-    )
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # letting client set up and start writing
-
-    logger.info("Checking writes are running at all...")
-    assert cw.count_znodes(
-        parent=parent, hosts=hosts_1, username=helpers.USERNAME, password=password_1
-    )
-
-    logger.info("Stopping continuous_writes...")
-    cw.stop_continuous_writes()
-
-    logger.info("Confirming writes on original cluster...")
-    assert cw.count_znodes(
-        parent=parent, hosts=hosts_1, username=helpers.USERNAME, password=password_1
-    )
-
-    logger.info("Confirming writes not replicated to new cluster...")
-    with pytest.raises(Exception):
-        cw.count_znodes(
-            parent=parent, hosts=hosts_2, username=helpers.USERNAME, password=password_2
-        )
-
-    logger.info("Cleaning up old cluster...")
-    await ops_test.model.applications[zk_2].remove()
-    await helpers.wait_idle(ops_test)
-
-
-async def test_full_cluster_crash(ops_test: OpsTest, request):
-    hosts = helpers.get_hosts(ops_test)
-    leader_name = helpers.get_leader_name(ops_test, hosts)
-    leader_host = helpers.get_unit_host(ops_test, leader_name)
-    password = helpers.get_super_password(ops_test)
-    parent = request.node.name
-    non_leader_hosts = ",".join([host for host in hosts.split(",") if host != leader_host])
-
-    logger.info("Extending pebble restart delay on all units...")
-    helpers.modify_pebble_restart_delay(ops_test, policy="extend")
-
-    logger.info("Checking writes are running at all...")
-    assert cw.count_znodes(
-        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
-    )
-
-    logger.info("Getting leader pid...")
-    current_pid = await helpers.get_pid(ops_test, leader_name)
-
-    logger.info("Killing leader process...")
-    await helpers.send_control_signal(ops_test=ops_test, unit_name=leader_name, signal="SIGKILL")
-    await asyncio.sleep(CLIENT_TIMEOUT)  # waiting for process to restore
-
-    logger.info("Checking leader pid changed...")
-    new_pid = await helpers.get_pid(ops_test, leader_name)
-    assert new_pid != current_pid  # validates process actually stopped
-
-    logger.info("Checking writes are increasing...")
-    writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # increasing writes
-    new_writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    assert new_writes > writes, "writes not continuing to ZK"
-
-    logger.info("Checking leader re-election...")
-    new_leader_name = helpers.get_leader_name(ops_test, non_leader_hosts)
-    assert new_leader_name != leader_name
-
-    logger.info("Stopping continuous_writes...")
-    cw.stop_continuous_writes()
-
-    logger.info("Counting writes on surviving units...")
-    last_write = cw.get_last_znode(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    total_writes = cw.count_znodes(
-        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
-    )
-    assert last_write == total_writes
-
-    logger.info("Checking old leader caught up...")
-    last_write_leader = cw.get_last_znode(
-        parent=parent, hosts=leader_host, username=helpers.USERNAME, password=password
-    )
-    total_writes_leader = cw.count_znodes(
-        parent=parent, hosts=leader_host, username=helpers.USERNAME, password=password
-    )
-    assert last_write == last_write_leader
-    assert total_writes == total_writes_leader
-
-
-@pytest.mark.abort_on_fail
 async def test_restart_db_process(ops_test: OpsTest, request):
     """SIGTERMSs leader process and checks recovery + re-election."""
     hosts = helpers.get_hosts(ops_test)
@@ -410,59 +224,6 @@ async def test_freeze_db_process(ops_test: OpsTest, request):
 
 
 @pytest.mark.abort_on_fail
-async def test_two_clusters_not_replicated(ops_test: OpsTest, request):
-    """Confirms that writes to one cluster are not replicated to another."""
-    zk_2 = f"{helpers.APP_NAME}2"
-
-    logger.info("Deploying second cluster...")
-    new_charm = await ops_test.build_charm(".")
-    await ops_test.model.deploy(
-        new_charm,
-        application_name=zk_2,
-        num_units=3,
-        resources={"zookeeper-image": helpers.ZOOKEEPER_IMAGE},
-        series=helpers.SERIES,
-    )
-    await helpers.wait_idle(ops_test, apps=[helpers.APP_NAME, zk_2])
-
-    parent = request.node.name
-
-    hosts_1 = helpers.get_hosts(ops_test)
-    hosts_2 = helpers.get_hosts(ops_test, app_name=zk_2)
-    password_1 = helpers.get_super_password(ops_test)
-    password_2 = helpers.get_super_password(ops_test, app_name=zk_2)
-
-    logger.info("Starting continuous_writes on original cluster...")
-    cw.start_continuous_writes(
-        parent=parent, hosts=hosts_1, username=helpers.USERNAME, password=password_1
-    )
-    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # letting client set up and start writing
-
-    logger.info("Checking writes are running at all...")
-    assert cw.count_znodes(
-        parent=parent, hosts=hosts_1, username=helpers.USERNAME, password=password_1
-    )
-
-    logger.info("Stopping continuous_writes...")
-    cw.stop_continuous_writes()
-
-    logger.info("Confirming writes on original cluster...")
-    assert cw.count_znodes(
-        parent=parent, hosts=hosts_1, username=helpers.USERNAME, password=password_1
-    )
-
-    logger.info("Confirming writes not replicated to new cluster...")
-    with pytest.raises(Exception):
-        cw.count_znodes(
-            parent=parent, hosts=hosts_2, username=helpers.USERNAME, password=password_2
-        )
-
-    logger.info("Cleaning up old cluster...")
-    await ops_test.model.applications[zk_2].remove()
-    await helpers.wait_idle(ops_test)
-
-
-@pytest.mark.abort_on_fail
 async def test_network_cut_without_ip_change(ops_test: OpsTest, request, chaos_mesh):
     """Cuts and restores network on leader, cluster self-heals after IP change."""
     hosts = helpers.get_hosts(ops_test)
@@ -530,6 +291,74 @@ async def test_network_cut_without_ip_change(ops_test: OpsTest, request, chaos_m
     assert total_writes == total_writes_leader
 
 
+@pytest.mark.abort_on_fail
+async def test_full_cluster_crash(ops_test: OpsTest, request):
+    hosts = helpers.get_hosts(ops_test)
+    leader_name = helpers.get_leader_name(ops_test, hosts)
+    leader_host = helpers.get_unit_host(ops_test, leader_name)
+    password = helpers.get_super_password(ops_test)
+    parent = request.node.name
+    non_leader_hosts = ",".join([host for host in hosts.split(",") if host != leader_host])
+
+    logger.info("Extending pebble restart delay on all units...")
+    helpers.modify_pebble_restart_delay(ops_test, policy="extend")
+
+    # letting the cluster settle
+    await helpers.wait_idle(ops_test)
+
+    logger.info("Starting continuous_writes...")
+    cw.start_continuous_writes(
+        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
+    )
+    await asyncio.sleep(CLIENT_TIMEOUT * 3)
+
+    logger.info("Counting writes are running at all...")
+    assert cw.count_znodes(
+        parent=parent, hosts=hosts, username=helpers.USERNAME, password=password
+    )
+
+    # kill all units "simultaneously"
+    await asyncio.gather(
+        *[
+            helpers.send_control_signal(ops_test, unit.name, signal="SIGKILL")
+            for unit in ops_test.model.applications[helpers.APP_NAME].units
+        ]
+    )
+
+    # Check that all servers are down at the same time before restoring them
+    try:
+        assert helpers.all_db_processes_down(ops_test), "Not all units down at the same time."
+    finally:
+        logger.info("Restoring pebble restart delay on all units...")
+        helpers.modify_pebble_restart_delay(ops_test, policy="restore")
+
+    # letting the cluster settle
+    await helpers.wait_idle(ops_test)
+
+    logger.info("Checking writes are increasing...")
+    writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
+    )
+    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # letting client set up and start writing
+    new_writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
+    )
+    assert new_writes > writes, "writes not continuing to ZK"
+
+    logger.info("Stopping continuous_writes...")
+    cw.stop_continuous_writes()
+
+    logger.info("Counting writes on surviving units...")
+    last_write = cw.get_last_znode(
+        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
+    )
+    total_writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=helpers.USERNAME, password=password
+    )
+    assert last_write == total_writes
+
+
+@pytest.mark.abort_on_fail
 async def test_full_cluster_restart(ops_test: OpsTest, request):
     hosts = helpers.get_hosts(ops_test)
     leader_name = helpers.get_leader_name(ops_test, hosts)
