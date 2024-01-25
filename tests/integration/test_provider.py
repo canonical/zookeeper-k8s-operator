ec80fca553d90f63c62a410453c3300357ea2462
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-01-25 23:42:02 +0000

    style: fmt

--- tests/integration/test_provider.py
+++ tests/integration/test_provider.py
@@ -117,9 +117,13 @@ async def test_scale_up_gets_new_jaas_users(ops_test: OpsTest):
 async def test_remove_applications(ops_test: OpsTest):
     async with ops_test.fast_forward():
         await ops_test.model.applications[DUMMY_NAME_1].remove()
-        await ops_test.model.wait_for_idle(apps=[APP_NAME], idle_period=30, status="active", timeout=1000)
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME], idle_period=30, status="active", timeout=1000
+        )
         await ops_test.model.applications[DUMMY_NAME_2].remove()
-        await ops_test.model.wait_for_idle(apps=[APP_NAME], idle_period=30, status="active", timeout=1000)
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME], idle_period=30, status="active", timeout=1000
+        )
 
     assert ping_servers(ops_test)
     for unit in ops_test.model.applications[APP_NAME].units:
