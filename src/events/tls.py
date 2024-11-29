2a394ddc4c8138a982a2147cdf910ad89d578c0d
Alex Batisse <alex.batisse@canonical.com>
2024-11-29 17:59:40 +0100

    chore: Bump certificates charm lib to v3

--- src/events/tls.py
+++ src/events/tls.py
@@ -9,9 +9,9 @@ import os
 import re
 from typing import TYPE_CHECKING
 
-from charms.tls_certificates_interface.v1.tls_certificates import (
+from charms.tls_certificates_interface.v3.tls_certificates import (
     CertificateAvailableEvent,
-    TLSCertificatesRequiresV1,
+    TLSCertificatesRequiresV3,
     generate_csr,
     generate_private_key,
 )
@@ -32,7 +32,7 @@ class TLSEvents(Object):
     def __init__(self, charm):
         super().__init__(charm, "tls")
         self.charm: "ZooKeeperCharm" = charm
-        self.certificates = TLSCertificatesRequiresV1(self.charm, "certificates")
+        self.certificates = TLSCertificatesRequiresV3(self.charm, "certificates")
 
         self.framework.observe(
             getattr(self.charm.on, "certificates_relation_created"), self._on_certificates_created
